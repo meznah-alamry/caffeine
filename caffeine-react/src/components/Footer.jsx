25da237c2952e481307fb662d4ee5675b926d28d
ASIM SAMI M ALLUHAYBI <eng.asimsami@gmail.com>
2021-01-21 02:30:38 +0300

    Working on Home Page

---
+++ caffeine-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function Footer() {
+    return (
+        <Navbar className="navbar footer-bar" variant="dark">
+        <Container >
+        <Nav className="mr-auto nav-bar-elements">
+          <Nav.Link href="#home">Contact Us</Nav.Link>
+          <Nav.Link href="#features"></Nav.Link>
+          <Nav.Link href="#pricing"></Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    )
+}
---
+++ caffeine-react/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from '../components/NavBar'
+import Section from '../components/Section'
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+  return (
+    <>
+      <NavBar />
+
+      <Section />
+
+      <Footer />
+
+    </>
+  );
+}

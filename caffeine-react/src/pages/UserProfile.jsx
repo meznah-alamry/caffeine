2b3b09d4fce3d117cf3947fe1565ff175ac42c66
talamajed <tala.majed.g@gmail.com>
2021-01-27 23:53:30 +0300

    profile image show

--- caffeine-react/src/pages/UserProfile.jsx
+++ caffeine-react/src/pages/UserProfile.jsx
@@ -1,35 +1,52 @@
 import React from "react";
 import axios from "axios";
-import  {useState}  from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react"; 
+
 export default function UserProfile(props) {
     
     const { name, email, _id } = props.auth.currentUser;
-   
+    const [UserForProfile, setUserForProfile] = useState({});
+
     const uploadImageHundler = (e) => {
         var format = new FormData()
         format.append("image", e.target.files[0])
         axios.post("https://api.imgur.com/3/image/", format, { headers: { "Authorization": "Client-ID {id}" } })
-          .then(data => console.log(data.data.data.link))
+        
+        .then(data => console.log(data.data.data.link))
+         
           .catch(err => console.log(err))
+          setUserForProfile(UserForProfile);
       } 
 
 
-      // useEffect(() => {
-      //   axios.get('http://localhost:5000/api/profile/')
-      //     .then(res => {
-      //       setArticles(res.data.msg.user)
-      //     })
-      // }, [])
-    
    
+    
+      useEffect(() => {
+         
+          axios.get("http://localhost:5000/api/user/users").then((res) => {
+            let UserForProfile = res.data.msg.find((ele) => ele._id == _id);
+            console.log("from user profile"+res.data.msg)
+            setUserForProfile(UserForProfile);
+          });
+        
+      }, []);
+
     return (
 
+
+
+
 <div className="cont">
+    
 <div className="form">
 <form>
           <h3>My Profile</h3>
+          <div className="profileIMG">  <img width="70" height="70" src={UserForProfile.img} alt="" srcset="" /></div>
           <hr/>
           <div className="form-group">
+
+        
               <label>Image:  </label>
         
               &nbsp;  <input type="file"></input>
